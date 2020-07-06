03f93ddec5fff29d6433d051fc8fd0d131e9aa2d
DevJavoxir <Java_0111_java@mail.ru>
2020-07-06 17:51:13 +0500

    nuka

--- resources/js/components/admin/customs/Customs.vue
+++ resources/js/components/admin/customs/Customs.vue
@@ -187,11 +187,31 @@
 					toast.fire({type: 'success',icon: 'success',title: 'Пользователь удалено!', })
 				}
 			},
+			ExcelDateToJSDate(serial) {
+				var utc_days  = Math.floor(serial - 25569);
+				var utc_value = utc_days * 86400;                                        
+				var date_info = new Date(utc_value * 1000);
+
+				var fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+				var total_seconds = Math.floor(86400 * fractional_day);
+
+				var seconds = total_seconds % 60;
+
+				total_seconds -= seconds;
+
+				var hours = Math.floor(total_seconds / (60 * 60));
+				var minutes = Math.floor(total_seconds / 60) % 60;
+
+				return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+			},
 			async sendExelData() {
-				if(this.form.length){
-					await this.actionImportExcel(this.form)
-				}
-				console.log(this.getMassage)
+				console.log(this.form)
+				// console.log(this.ExcelDateToJSDate(43831))
+
+				// if(this.form.length){
+				// 	await this.actionImportExcel(this.form)
+				// }
 			},
 			previewFiles(e) {
 				this.loading = true
